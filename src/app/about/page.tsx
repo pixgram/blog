58090c87f5eafad6bcc9e9f1d9dba3827cb4adc3
rsp <pixgram@gmail.com>
2023-09-21 18:01:45 +0900

    add send email

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,34 @@
+import Hero from "@/components/Hero";
+
+const TITLE_CLASS = "text-2xl font-bold text-gray-800 m-2";
+
 export default function AboutPage() {
-  return <p>about page</p>;
+  return (
+    <>
+      <Hero />
+      <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          개발을 사랑하는 풀스택 개발자
+          <br />
+        </p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>커리어</p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>
+          WebGL, GLSL
+          <br />
+          React, Vue, Node
+          <br />
+          Git, GitLab, BitBucket
+          <br />
+          TypeScript, jQuery, three.js
+          <br />
+          VS Code, Intellij, pyCham
+          <br />
+          python
+        </p>
+      </section>
+    </>
+  );
 }
